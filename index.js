79d9fb2b38d44ac18dc051ad7722206f782ca196
Thokozani Nqwili <simthokozile@gmail.com>
2022-11-26 11:35:14 +0200

    add

--- index.js
+++ index.js
@@ -3,70 +3,65 @@ let operator = document.getElementById("dot");
 let value = document.getElementsByClassName("value");
 let result = document.getElementById("result").innerHTML;
 let num = []; // array to store values
-let allOperators = document.getElementsByClassName('operator');
+let allOperators = document.getElementsByClassName("operator");
 
 function evaluateSum() {
   let t = document.getElementById("result").innerHTML;
-  sum = eval(t);
-  num = ['.']
-  console.log(num)
+  sum = eval(t.replace(/\s/g, "")).toFixed(2);
+  num = ["."];
+  
 
   if (sum === undefined) {
-    document.getElementById("result2").innerHTML = " ";
+    document.getElementById("result2").innerHTML = "";
   } else {
     document.getElementById("result2").innerHTML = sum;
   }
 }
 
+
 function clearSum() {
   document.getElementById("result").innerHTML = " ";
   document.getElementById("result2").innerHTML = " ";
-  num =[]
-  console.log(num)
+  num = [];
+  
 }
 
 function addToSum(value) {
-  switch(value){
-      case `+`:
-        case `-`:
-          case `*`:
-            case `/`:
-                num = []
-                console.log(num)
-                document.getElementById("result").innerHTML += value;
+  switch (value) {
+    case `+`:
+    case `-`:
+    case `*`:
+    case `/`:
+      num = [];
+      
+      document.getElementById("result").innerHTML += value;
 
-                break;
-                default:
-                  document.getElementById("result").innerHTML += value;
-                  num.push(value)
-                  console.log(num)
-
+      break;
+    default:
+      document.getElementById("result").innerHTML += value;
+      num.push(value);
+     
+  }
 }
-}
 
-  
-
-function decimal(){
-  if(num.includes(operator.innerHTML)){ //checks to see if a dot is already in the array
-    document.getElementById('result').innerHTML = document.getElementById('result').innerHTML 
-    console.log("step reached")
-  } else{
-    document.getElementById('result').innerHTML += operator.innerHTML
-    num.push(operator.innerHTML) // adds to the num array
-    console.log(num)
+function decimal() {
+  if (num.includes(operator.innerHTML)) { //checks to see if a dot is already in the array
+    document.getElementById("result").innerHTML =
+      document.getElementById("result").innerHTML;
+    
+  } else {
+    document.getElementById("result").innerHTML += operator.innerHTML;
+    num.push(operator.innerHTML); // adds to the num array
+    
   }
 }
 
 function del() {
-    document.getElementById("result").innerHTML = document
-      .getElementById("result")
-      .innerHTML.slice(0, -1);
-      num.pop() // removes the last value in the num array
-      console.log(num)
+  document.getElementById("result").innerHTML = document
+    .getElementById("result")
+    .innerHTML.slice(0, -1);
+  num.pop(); // removes the last value in the num array
   
 }
 
 
-// console.log(e)
-console.log(operator.innerHTML)
-console.log()
